3d57d49fe62d82fb97ab3ec79e1b6e2ed8344f75
Chizhov Anton <zr9ihi@gmail.com>
2023-11-27 22:06:01 +0300

    Add a file with all datasets info

---
+++ src/tests/all_datasets_info.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "datasets.h"
+
+namespace tests {
+
+static inline const DatasetInfo kWDC_astronomical = {"WDC_astronomical.csv", ',', true};
+static inline const DatasetInfo kWDC_symbols = {"WDC_symbols.csv", ',', true};
+static inline const DatasetInfo kWDC_science = {"WDC_science.csv", ',', true};
+static inline const DatasetInfo kWDC_satellites = {"WDC_satellites.csv", ',', true};
+static inline const DatasetInfo kWDC_appearances = {"WDC_appearances.csv", ',', true};
+static inline const DatasetInfo kWDC_astrology = {"WDC_astrology.csv", ',', true};
+static inline const DatasetInfo kWDC_game = {"WDC_game.csv", ',', true};
+static inline const DatasetInfo kWDC_kepler = {"WDC_kepler.csv", ',', true};
+static inline const DatasetInfo kWDC_planetz = {"WDC_planetz.csv", ',', true};
+static inline const DatasetInfo kWDC_age = {"WDC_age.csv", ',', true};
+static inline const DatasetInfo kTestWide = {"TestWide.csv", ',', true};
+static inline const DatasetInfo kabalone = {"abalone.csv", ',', true};
+static inline const DatasetInfo kiris = {"iris.csv", ',', false};
+static inline const DatasetInfo kadult = {"adult.csv", ';', false};
+static inline const DatasetInfo kbreast_cancer = {"breast_cancer.csv", ',', true};
+static inline const DatasetInfo kCIPublicHighway10k = {"CIPublicHighway10k.csv", ',', true};
+static inline const DatasetInfo kneighbors10k = {"neighbors10k.csv", ',', true};
+static inline const DatasetInfo kneighbors50k = {"neighbors50k.csv", ',', true};
+static inline const DatasetInfo kneighbors100k = {"neighbors100k.csv", ',', true};
+static inline const DatasetInfo kCIPublicHighway700 = {"CIPublicHighway700.csv", ',', true};
+static inline const DatasetInfo kEpicVitals = {"EpicVitals.csv", '|', true};
+static inline const DatasetInfo kEpicMeds = {"EpicMeds.csv", '|', true};
+static inline const DatasetInfo kiowa1kk = {"iowa1kk.csv", ',', true};
+static inline const DatasetInfo kfd_reduced_30 = {"fd-reduced-30.csv", ',', true};
+static inline const DatasetInfo kflight_1k = {"flight_1k.csv", ';', true};
+static inline const DatasetInfo kplista_1k = {"plista_1k.csv", ';', false};
+static inline const DatasetInfo kletter = {"letter.csv", ',', false};
+static inline const DatasetInfo kCIPublicHighway = {"CIPublicHighway.csv", ',', true};
+static inline const DatasetInfo kLegacyPayors = {"LegacyPayors.csv", '|', true};
+
+}  // namespace tests
